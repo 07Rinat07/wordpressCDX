6b8bfb9c2c2b3ebd7bda3dc952953b150927045a
Rinat Sarmuldin <ura07srr@gmail.com>
2024-11-27 18:15:24 +0500

    Update WP and Plugin Elementor and Registration Post Type. #1 A

--- wp-content/themes/wordpresscdx/functions.php
+++ wp-content/themes/wordpresscdx/functions.php
@@ -54,6 +54,53 @@ add_action('after_setup_theme', 'wordpresscdx_theme_init', 0);
 
 
 
+function wordpresscdx_register_post_type(){
+	$args = array(
+		'label' => esc_html__('Cars', 'wordpresscdx'),
+		'labels' => array(
+			'name'                  => esc_html_x( 'Books', 'Post type general name', 'textdomain' ),
+		'singular_name'         => esc_html_x( 'Book', 'Post type singular name', 'textdomain' ),
+		'menu_name'             => esc_html_x( 'Books', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar'        => esc_html_x( 'Book', 'Add New on Toolbar', 'textdomain' ),
+		'add_new'               => esc_html__( 'Add New', 'textdomain' ),
+		'add_new_item'          => esc_html__( 'Add New Book', 'textdomain' ),
+		'new_item'              => esc_html__( 'New Book', 'textdomain' ),
+		'edit_item'             => esc_html__( 'Edit Book', 'textdomain' ),
+		'view_item'             => esc_html__( 'View Book', 'textdomain' ),
+		'all_items'             => esc_html__( 'All Books', 'textdomain' ),
+		'search_items'          => esc_html__( 'Search Books', 'textdomain' ),
+		'parent_item_colon'     => esc_html__( 'Parent Books:', 'textdomain' ),
+		'not_found'             => esc_html__( 'No books found.', 'textdomain' ),
+		'not_found_in_trash'    => esc_html__( 'No books found in Trash.', 'textdomain' ),
+		'featured_image'        => esc_html_x( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'set_featured_image'    => esc_html_x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'remove_featured_image' => esc_html_x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'use_featured_image'    => esc_html_x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'archives'              => esc_html_x( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+		'insert_into_item'      => esc_html_x( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+		'uploaded_to_this_item' => esc_html_x( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+		'filter_items_list'     => esc_html_x( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+		'items_list_navigation' => esc_html_x( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+		'items_list'            => esc_html_x( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+		),
+		'supports' => array('title', 'editor', 'author', 'thumbnail'),
+		'public' => true,
+		'public_queryable' => true, //сдеелает во фронте отображение по ссылке
+		'show_ui' => true, // включит в админ панеле внешний интерфейс
+		'show_in_menu' => true, //включит ссылку на новый посттайп
+		'has_archive' => true,
+	
+	);
+	register_post_type('car', $args);
+
+}
+add_action('init', 'wordpresscdx_register_post_type');
+
+
+
+
+
+
 
 
 
